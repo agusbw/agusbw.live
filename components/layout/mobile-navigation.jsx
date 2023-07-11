e026a25d2d859e55029f27966956c0560ab0688e
agusbw <agusbudhiarta81@gmail.com>
2023-07-12 02:19:22 +0800

    feat: add navigation bar

--- components/layout/mobile-navigation.jsx
+++ components/layout/mobile-navigation.jsx
@@ -1,5 +1,57 @@
+"use client";
+
+import TextLink from "../shared/text-link";
+import { useState } from "react";
+
+const HamburgerMenu = ({ handleNavToggle }) => {
+  return (
+    <button
+      className="p-3 rounded-md focus:outline outline-bdr"
+      onClick={handleNavToggle}
+    >
+      <div className="flex flex-col gap-1">
+        {Array(3)
+          .fill(undefined)
+          .map((v, i) => (
+            <span
+              key={i}
+              className="block after:h-[2px] rounded-lg after:block after:w-7 bg-lowContrast cursor-pointer"
+            ></span>
+          ))}
+      </div>
+    </button>
+  );
+};
+
 const MobileNavigation = () => {
-  return <div>Mobile navigation</div>;
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavigationOpen((prev) => !prev);
+  };
+
+  return (
+    <nav className="sticky top-0 py-2 mx-8 border-b sm:hidden backdrop-blur-lg border-b-bdr">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-5">
+          <div className="flex items-center justify-center w-16 h-16 border-2 rounded-full cursor-pointer bg-primary text-md border-bdr">
+            <p>B W</p>
+          </div>
+          <TextLink href={"/"}>Get in touch</TextLink>
+        </div>
+        <div className="relative">
+          <HamburgerMenu handleNavToggle={handleNavToggle} />
+          {isNavigationOpen && (
+            <div className="absolute right-0 flex flex-col items-end py-2 mt-2 border rounded-md ps-16 pe-2 border-bdr bg-base">
+              <TextLink href={"/about"}>About</TextLink>
+              <TextLink href={"/about"}>Projects</TextLink>
+              <TextLink href={"/about"}>Blog</TextLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default MobileNavigation;
--- components/layout/top-navigation.jsx
+++ components/layout/top-navigation.jsx
@@ -1,23 +1,28 @@
 import TextLink from "../shared/text-link";
+import MobileNavigation from "./mobile-navigation";
 
 const TopNavigation = () => {
   return (
-    <nav className="lg:px-56 lg:py-8">
-      <div className="flex justify-between">
-        <div className="flex items-center justify-center w-8 h-8 p-8 border-2 rounded-full text-md border-bdr">
-          <p>Email</p>
+    <>
+      <nav className="sticky top-0 hidden border-b sm:block backdrop-blur-lg border-b-bdr sm:mx-20 sm:py-3 lg:mx-56 lg:py-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="flex items-center justify-center w-16 h-16 border-2 rounded-full cursor-pointer bg-primary text-md border-bdr">
+              <p>B W</p>
+            </div>
+            <TextLink href={"/"}>Get in touch</TextLink>
+          </div>
+          <div className="flex gap-2">
+            <TextLink href={"/"}>About</TextLink>
+            <span>/</span>
+            <TextLink href={"/"}>Projects</TextLink>
+            <span>/</span>
+            <TextLink href={"/"}>Blog</TextLink>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <TextLink href={"/"}>Home</TextLink>
-          <span>/</span>
-          <TextLink href={"/"}>About</TextLink>
-          <span>/</span>
-          <TextLink href={"/"}>Projects</TextLink>
-          <span>/</span>
-          <TextLink href={"/"}>Blog</TextLink>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <MobileNavigation />
+    </>
   );
 };
 export default TopNavigation;
