a27b57559ba2692700de2ee137e35d8db5aaad47
agusbw <agusbudhiarta81@gmail.com>
2023-07-15 00:45:45 +0800

    fix: change element tag

--- components/shared/badge.jsx
+++ components/shared/badge.jsx
@@ -4,6 +4,6 @@ const baseClass =
   "py-0.5 px-3 text-sm rounded-full border text-center border-bdr bg-base";
 
 const Badge = ({ children, className }) => {
-  return <div className={twMerge(baseClass, className)}>{children}</div>;
+  return <span className={twMerge(baseClass, className)}>{children}</span>;
 };
 export default Badge;
