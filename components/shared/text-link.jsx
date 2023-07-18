59a994cd4cd9530ea8ad632844f46cc9c8452b5f
agusbw <agusbudhiarta81@gmail.com>
2023-07-19 02:17:26 +0800

    feat: add external link variant to text link

--- components/shared/text-link.jsx
+++ components/shared/text-link.jsx
@@ -4,11 +4,25 @@ import { twMerge } from "tailwind-merge";
 const baseClass =
   "transition-colors duration-100 text-lowContrast hover:text-highContrast";
 
-const TextLink = ({ href, children, className, ...props }) => {
+const TextLink = ({ href, children, className, isExternal, ...props }) => {
+  if (isExternal) {
+    return (
+      <Link
+        className={twMerge(baseClass, className)}
+        target="_blank"
+        href={href}
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <Link
       className={twMerge(baseClass, className)}
       href={href}
+      {...props}
     >
       {children}
     </Link>
