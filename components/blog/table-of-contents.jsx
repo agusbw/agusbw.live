f96678c639baf5da882eeefb4ef5a311d0543764
agusbw <agusbudhiarta81@gmail.com>
2023-07-19 22:35:04 +0800

    feat: change TOC style

--- components/blog/table-of-contents.jsx
+++ components/blog/table-of-contents.jsx
@@ -1,24 +1,21 @@
 import TOClink from "../shared/toc-link";
-import GithubSlugger from "github-slugger";
 
 const TableOfContents = ({ headings }) => {
   return (
     <div className="sticky hidden text-left shrink-0 xl:block h-fit top-32">
-      <p className="text-xl font-semibold text-highContrast after:block after:w-3/4 after:h-0.5  after:bg-highContrast">
+      <p className="text-xl font-semibold text-highContrast after:block after:w-3/4 after:h-0.5  after:bg-highContrast ">
         Table of Contents
       </p>
-      <ul className="mt-2 text-sm list-disc list-inside">
+      <ul className="mt-2 text-sm">
         {headings.map((heading) => {
           return (
             <li
               key={heading.slug}
-              data-level={heading.level}
-              className="data-[level=three]:pl-5 "
+              className="mb-1"
             >
               <TOClink
-                href={`#${heading.slug}`}
-                headingId={heading.slug}
-                className={"hover:text-highContrast"}
+                headingId={`${heading.slug}`}
+                data-level={heading.level}
               >
                 {heading.text}
               </TOClink>
--- components/shared/toc-link.jsx
+++ components/shared/toc-link.jsx
@@ -1,11 +1,40 @@
 "use client";
 
+import * as React from "react";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
-const baseClass = "";
+const baseClass =
+  "hover:text-highContrast pl-2 data-[level=three]:ml-3  relative before:content-[''] before:absolute before:inset-y-0.5 before:left-0 before:transition-all   before:duration-300 before:ease-out";
 
-const TOClink = ({ children, className, headingId, href, ...props }) => {
+function useHighlighted(id) {
+  const observer = React.useRef();
+  const [activeId, setActiveId] = React.useState("");
+
+  React.useEffect(() => {
+    const handleObserver = (entries) => {
+      entries.forEach((entry) => {
+        if (entry?.isIntersecting) {
+          setActiveId(entry.target.id);
+        }
+      });
+    };
+
+    observer.current = new IntersectionObserver(handleObserver, {
+      rootMargin: "0% 0% -80% 0px",
+    });
+
+    const elements = document.querySelectorAll("h2, h3");
+    elements.forEach((elem) => observer.current.observe(elem));
+    return () => observer.current?.disconnect();
+  }, []);
+
+  return [activeId === id, setActiveId];
+}
+
+const TOClink = ({ children, className, headingId, ...props }) => {
+  const [highlighted, setHighlighted] = useHighlighted(headingId);
+
   const handleOnClick = (e) => {
     e.preventDefault();
     const topOfElements = document.getElementById(headingId).offsetTop - 105;
@@ -13,13 +42,24 @@ const TOClink = ({ children, className, headingId, href, ...props }) => {
       top: topOfElements,
       behavior: "smooth",
     });
+    setHighlighted(headingId);
   };
 
   return (
     <Link
-      href={href}
-      className={twMerge(baseClass, className)}
-      onClick={(e) => handleOnClick(e)}
+      href={`#${headingId}`}
+      className={twMerge(
+        baseClass,
+        className,
+        ` ${
+          highlighted
+            ? "before:w-1 before:bg-highContrast text-highContrast"
+            : "before:w-0"
+        }`
+      )}
+      onClick={(e) => {
+        handleOnClick(e);
+      }}
       {...props}
     >
       {children}
